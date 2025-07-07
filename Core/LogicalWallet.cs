b2bb223a63f1c40d4749f9df731f48f847fcb5ae
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2025-07-07 19:27:58 +0200

    PhysicalWallet

--- Core/LogicalWallet.cs
+++ Core/LogicalWallet.cs
@@ -5,10 +5,19 @@ namespace FinanceTracker.Core;
 /// but is rather a "fund" that can be allocated in different physical wallets.
 /// </summary>
 /// <param name="Name">User-friendly name of the wallet.</param>
-/// <param name="Value">Current value of the wallet.</param>
+/// <param name="Allocations">Allocations in the physical wallets.</param>
 /// <param name="Target">Target value of the wallet. Null if unspecified.</param>
 public record LogicalWallet(
     string Name,
-    Money Value,
-    Money? Target = null
-    );
+    Dictionary<PhysicalWallet, decimal> Allocations,
+    decimal? Target = null
+)
+{
+    public decimal CalculateValue(IReadOnlyDictionary<string, decimal> conversions)
+    {
+        return Allocations
+            .Select(allocation => conversions[allocation.Key.Currency] * allocation.Value)
+            .Sum();
+    }
+}
+    
