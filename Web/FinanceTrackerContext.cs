e1f3553a8ad60ff7100507cdad01a7466c1630b3
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2025-07-23 21:01:41 +0200

    Ready for code review

--- Web/FinanceTrackerContext.cs
+++ Web/FinanceTrackerContext.cs
@@ -60,47 +60,4 @@ public class FinanceTrackerContext(DbContextOptions<FinanceTrackerContext> optio
                 b.Property(x => x.DisplaySequence);
             });
     }
-
-    public static Portfolio BuildTestPortfolio()
-    {
-        var date = new DateOnly(2020, 1, 1);
-        
-        var emergencyFund = new Wallet("Emergency Fund", displaySequence: 1);
-        AddComponents(emergencyFund, date, ["Savings Account", "Cash - PLN", "Cash - CAD", "Bonds", "Bonds - Retirement Account"]);
-        
-        var longTermWallet = new Wallet("Long-term Wallet", displaySequence: 2);
-        AddComponents(longTermWallet, date, ["Bonds", "Stocks"]);
-
-        var home = new Asset("Home", displaySequence: 1);
-        home.Evaluate(date, new Money(0, "PLN", 0));
-        
-        var car = new Asset("Car", displaySequence: 2);
-        car.Evaluate(date, new Money(0, "PLN", 0));
-        
-        var mortgage = new Debt("Mortgage", displaySequence: 1);
-        mortgage.Evaluate(date, new Money(0, "PLN", 0));
-        
-        var dryer = new Debt("Dryer", displaySequence: 2);
-        dryer.Evaluate(date, new Money(0, "PLN", 0));
-        
-        var portfolio = new Portfolio(name: "My portfolio");
-        portfolio.Add(emergencyFund);
-        portfolio.Add(longTermWallet);
-        portfolio.Add(home);
-        portfolio.Add(car);
-        portfolio.Add(mortgage);
-        portfolio.Add(dryer);
-
-        return portfolio;
-    }
-
-    private static void AddComponents(Wallet wallet, DateOnly date, IEnumerable<string> names)
-    {
-        foreach (var (name, displaySequence) in names.Zip(Enumerable.Range(1, 5)))
-        {
-            var component = new Component(name, displaySequence);
-            component.Evaluate(date, new Money(0, "PLN", 0));
-            wallet.Add(component);
-        }
-    }
 }
