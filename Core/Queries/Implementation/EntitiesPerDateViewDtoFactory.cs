586f3fd577fa0a0fb33816a8fbfd47d96cc38305
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2025-07-27 11:36:22 +0200

    Fix

--- Core/Queries/Implementation/EntitiesPerDateViewDtoFactory.cs
+++ Core/Queries/Implementation/EntitiesPerDateViewDtoFactory.cs
@@ -27,9 +27,9 @@ internal static class EntitiesPerDateViewDtoFactory
             Data: dates
                 .Select(date => BuildEntitiesForDateDto(
                         date: date,
-                        valueSnapshots: entities
+                        entityValues: entities
                             .OrderBy(entity => entity.DisplaySequence)
-                            .Select(entity => new ValueSnapshotDto(
+                            .Select(entity => new EntityValueDto(
                                 Name: entity.Name,
                                 Value: GetValueFor(entity, date, valueType),
                                 Id: entity.Id
@@ -43,27 +43,32 @@ internal static class EntitiesPerDateViewDtoFactory
         );
     }
 
-    private static decimal GetValueFor(this IEntityWithValueHistory entity, DateOnly date, BaseValueType valueType)
+    private static ValueSnapshotDto? GetValueFor(this IEntityWithValueHistory entity, DateOnly date, BaseValueType valueType)
     {
-        var value = Math.Abs(entity.GetValueFor(date) ?? 0);
+        var decimalValue = entity.GetValueFor(date);
 
-        return valueType switch
-        {
-            BaseValueType.Positive => value,
-            BaseValueType.Negative => -value,
-            _ => throw new NotImplementedException()
-        };
+        return decimalValue.HasValue
+            ? new ValueSnapshotDto(
+                Value: valueType switch
+                {
+                    BaseValueType.Positive => Math.Abs(decimalValue.Value),
+                    BaseValueType.Negative => -Math.Abs(decimalValue.Value),
+                    _ => throw new NotImplementedException()
+                })
+            : null;
     }
     
     private static EntitiesForDateDto BuildEntitiesForDateDto(
         DateOnly date,
-        IReadOnlyCollection<ValueSnapshotDto> valueSnapshots) =>
+        IReadOnlyCollection<EntityValueDto> entityValues) =>
         new(
             Date: date,
-            Entities: valueSnapshots,
-            Summary: new ValueSnapshotDto(
+            Entities: entityValues,
+            Summary: new EntityValueDto(
                 Name: "Summary",
-                Value: valueSnapshots.Sum(entity => entity.Value)
+                Value: new ValueSnapshotDto(
+                    Value: entityValues.Sum(entity => entity.Value?.Value ?? 0)
+                )
             )
         );
 
@@ -78,13 +83,31 @@ internal static class EntitiesPerDateViewDtoFactory
 
     private static EntitiesForDateDto CalculateChanges(EntitiesForDateDto previous, EntitiesForDateDto current)
     {
+        var entities = previous.Entities
+            .Zip(current.Entities)
+            .Select(pair => pair.Second with
+            {
+                Value = ValueSnapshotDto.CalculateChanges(pair.First.Value, pair.Second.Value)
+            })
+            .ToArray();
+        
+        var changeSum = entities.Sum(x => x.Value?.Change ?? 0);
+
+        var summary = current.Summary.Value != null
+            ? current.Summary.Value with
+            {
+                Change = changeSum,
+                CumulativeChange = changeSum + (previous.Summary.Value?.CumulativeChange ?? 0)
+            }
+            : null;
+        
         return current with
         {
-            Entities = previous.Entities
-                .Zip(current.Entities)
-                .Select(pair => ValueSnapshotDto.CalculateChanges(pair.First, pair.Second))
-                .ToArray(),
-            Summary = ValueSnapshotDto.CalculateChanges(previous.Summary, current.Summary)
+            Entities = entities,
+            Summary = current.Summary with
+            {
+                Value = summary
+            }
         };
     }
 }
