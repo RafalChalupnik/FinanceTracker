8244e11a1fa31afe1a49de0825ea3fdd4aa65142
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2025-08-03 22:24:58 +0200

    Charts

--- Web/ClientApp/src/pages/Wallets.tsx
+++ Web/ClientApp/src/pages/Wallets.tsx
@@ -9,6 +9,10 @@ import {
     WalletValueHistory
 } from "../api/ValueHistoryApi";
 import EmptyConfig from "../components/EmptyConfig";
+import {CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis} from "recharts";
+
+const chartLineColors = ['#1890ff', '#52c41a', '#f5222d', '#fa8c16', '#722ed1', '#13c2c2'];
+
 
 interface WalletsProps {}
 
@@ -42,15 +46,51 @@ const Wallets: FC<WalletsProps> = (props) => {
             <EmptyConfig enabled={wallets.length === 0}>
                 <Space direction="vertical">
                     {wallets.map(wallet =>
-                        <EditableMoneyTable
-                            title={wallet.name}
-                            rows={wallet.data}
-                            columns={wallet.headers}
-                            editable={{
-                                onUpdate: updateComponent,
-                                onDelete: date => deleteEvaluations(wallet.id, date),
-                            }}
-                        />
+                    {
+                        let series = wallet.headers.map((header, index) => {
+                            return {
+                                name: header.name,
+                                data: wallet.data.map(dataPoint => {
+                                    return {
+                                        date: dataPoint.date,
+                                        value: dataPoint.components[index]?.value.amountInMainCurrency ?? 0
+                                    }
+                                })
+                            }
+                        })
+                        
+                        return (
+                            <Space direction="vertical">
+                                <EditableMoneyTable
+                                    title={wallet.name}
+                                    rows={wallet.data}
+                                    columns={wallet.headers}
+                                    editable={{
+                                        onUpdate: updateComponent,
+                                        onDelete: date => deleteEvaluations(wallet.id, date),
+                                    }}
+                                />
+                                <ResponsiveContainer width="100%" height={300}>
+                                    <LineChart width={500} height={300}>
+                                        <CartesianGrid strokeDasharray="3 3" />
+                                        <XAxis dataKey="date" type="category" allowDuplicatedCategory={false} />
+                                        <YAxis dataKey="value" />
+                                        <Tooltip />
+                                        <Legend />
+                                        {series.map((s, idx) => (
+                                            <Line 
+                                                dataKey="value" 
+                                                data={s.data} 
+                                                name={s.name} 
+                                                key={s.name}
+                                                stroke={chartLineColors[idx % chartLineColors.length]}
+                                            />
+                                        ))}
+                                    </LineChart>
+                                </ResponsiveContainer>
+                            </Space>
+                        );
+                    }
                     )}
                 </Space>
             </EmptyConfig>
