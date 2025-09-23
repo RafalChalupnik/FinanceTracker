6d759561c6c87bc9ff356f9aff873951d85b59e1
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2025-09-23 21:04:50 +0200

    Added electron.js

---
+++ Web/ClientApp/public/electron.js
@@ -0,0 +1,48 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false
+        }
+    });
+
+    const startUrl = app.isPackaged
+        ? url.format({
+            pathname: path.join(__dirname, '..', 'build', 'index.html'),
+            protocol: 'file:',
+            slashes: true,
+        })
+        : 'http://localhost:3000';
+
+    mainWindow.loadURL(startUrl);
+
+    if (!app.isPackaged) {
+        mainWindow.webContents.openDevTools();
+    }
+
+    mainWindow.on('closed', function () {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', function () {
+    if (mainWindow === null) {
+        createWindow();
+    }
+});
