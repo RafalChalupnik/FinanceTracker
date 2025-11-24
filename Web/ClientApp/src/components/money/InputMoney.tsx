79d4845cf6adad708be45529661c8f2a8ca10841
Rafal Chalupnik <rafal.chalupnik@gmail.com>
2025-11-24 19:55:15 +0100

    WiP: InputMoney

--- Web/ClientApp/src/components/money/InputMoney.tsx
+++ Web/ClientApp/src/components/money/InputMoney.tsx
@@ -1,7 +1,89 @@
-import React, {FC, useState} from "react";
-import {Space} from "antd";
-import InputCurrency from "./InputCurrency";
+import React, {FC, ReactNode, useState} from "react";
+import {InputNumber, Select, Space} from "antd";
 import {TransactionOutlined} from "@ant-design/icons";
+
+const { Option } = Select;
+
+interface InputCurrencyProps {
+    value?: number;
+    currency?: string;
+    currencyOptions: string[];
+    disableCurrencyPicker?: boolean;
+    onValueChange: (value: number | undefined) => void;
+    onCurrencyChange?: (currency: string) => void;
+    extra?: ReactNode;
+}
+
+const InputCurrency: FC<InputCurrencyProps> = (props) => {
+    const [focused, setFocused] = useState(false);
+    const [currency, setCurrency] = useState(props?.currency);
+
+    const formatter = (val?: number | string) => {
+        if (val === undefined || val === null || val === "") return "";
+        const number = typeof val === "number" ? val : parseFloat(val.replace(/[^\d.-]/g, ""));
+        return focused
+            ? number.toString()
+            : new Intl.NumberFormat(navigator.language, {
+                style: "currency",
+                currency: currency,
+                minimumFractionDigits: 2,
+                maximumFractionDigits: 2,
+            }).format(number);
+    };
+
+    const parser = (val?: string): number => {
+        if (!val) return 0;
+        const numeric = val.replace(/[^\d.-]/g, "");
+        return numeric ? parseFloat(numeric) : 0;
+    };
+
+    const buildAddOn = () => {
+        if (props.extra) {
+            return props.extra;
+        }
+
+        if (props?.currency === undefined)
+        {
+            return undefined;
+        }
+
+        return (
+            <Select
+                disabled={props.disableCurrencyPicker ?? false}
+                defaultValue={currency}
+                style={{ width: "auto", minWidth: 80 }}
+                onChange={value => {
+                    setCurrency(value);
+                    props.onCurrencyChange?.(value);
+                }}
+                showSearch
+                optionFilterProp="children"
+            >
+                {props.currencyOptions.map(c => (
+                    <Option key={c} value={c}>
+                        {c}
+                    </Option>
+                ))}
+            </Select>
+        );
+    }
+    
+    return (
+        <InputNumber
+            value={props.value}
+            style={{ width: 'auto' }}
+            step={0.01}
+            formatter={formatter}
+            parser={parser}
+            onChange={(val) => {
+                props.onValueChange(val ?? undefined);
+            }}
+            onFocus={() => setFocused(true)}
+            onBlur={() => setFocused(false)}
+            addonAfter={buildAddOn()}
+        />
+    );
+}
 
 interface InputMoneyProps {
     
@@ -9,6 +91,7 @@ interface InputMoneyProps {
 
 const InputMoney : FC<InputMoneyProps> = (props) => {
     const MAIN_CURRENCY = 'PLN';
+    const AVAILABLE_CURRENCIES = ["PLN", "CAD", "USD", "EUR", "GBP"];
 
     const [amount, setAmount] = useState<number | undefined>(123);
     const [currency, setCurrency] = useState('PLN');
@@ -25,25 +108,20 @@ const InputMoney : FC<InputMoneyProps> = (props) => {
     return (
         <Space direction='vertical'>
             <InputCurrency 
+                value={amount}
+                currency={currency}
+                currencyOptions={AVAILABLE_CURRENCIES}
+                disableCurrencyPicker={false}
                 onValueChange={setAmount}
-                initialValue={amount}
-                currency={{
-                    onChange: currency => {
-                        setCurrency(currency);
-
-                        if (currency !== MAIN_CURRENCY && amountInMainCurrency === undefined) {
-                            setAmountInMainCurrency(0);
-                        }
-                    },
-                    initialCurrency: currency
-                }}
+                onCurrencyChange={setCurrency}
             />
             {currency !== MAIN_CURRENCY && <InputCurrency
+                value={amountInMainCurrency}
+                currency={MAIN_CURRENCY}
+                currencyOptions={[]}
+                disableCurrencyPicker={true}
                 onValueChange={setAmountInMainCurrency}
-                initialValue={amountInMainCurrency}
-                currency={{
-                    disableCurrencyPicker: true
-                }}
+                onCurrencyChange={() => {}}
                 extra={<TransactionOutlined onClick={convertCurrency}/>}
             />}
         </Space>
